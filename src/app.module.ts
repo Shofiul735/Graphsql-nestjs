7844fd6d64f7422a6509b1ebc4b3583b2bc8f3c8
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-17 17:34:02 +0600

    Apollo driver added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: true
-  })]
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
 })
 export class AppModule {}
